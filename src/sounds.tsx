dc6a70c76564671ca8fb9d0bc4a6affe73965773
Gagan Saroya <gagan@saroya.me>
2023-03-12 23:20:48 -0400

    Use html5 for sound

--- src/sounds.tsx
+++ src/sounds.tsx
@@ -3,18 +3,22 @@ import { Howl, Howler } from 'howler';
 const sounds = {
   clickSound: new Howl({
     src: ["/audio/click.mp3"],
+    html5: true
   }),
   logonSound: new Howl({
     src: ["/audio/logon.mp3"],
     volume: 0.8,
+    html5: true
   }),
   logoffSound: new Howl({
     src: ["/audio/logoff.mp3"],
     volume: 0.4,
+    html5: true
   }),
   popSound: new Howl({
     src: ["/audio/pop1.mp3"],
     volume: 0.8,
+    html5: true
   }),
 }
 
